662b25699cedfe2cd9ccc270d675909faf3891f1
Igor Gladkoborodov <igor.gladkoborodov@gmail.com>
2020-07-28 22:17:53 -0700

    Autorun

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-rm -rf ~/.config/autorun
-ln -s ${PWD}/autorun ~/.config/autorun
-# TODO: launch daemon
-
+echo "Setup ~/.zshrc"
 rm ~/.zshrc
-ln -s ${PWD}/zsh/_zshrc ~/.zshrc
-touch ~/.zshrc.local_
+ln -s ${PWD}/zsh/zshrc ~/.zshrc
 
+echo "Touch ~/.zshrc.local, use it for local zsh stuff"
+touch ~/.zshrc.local
+
+echo "Setup MTMR, please restart it"
 rm ~/Library/Application\ Support/MTMR/items.json
 ln -s ${PWD}/mtmr/items.json ~/Library/Application\ Support/MTMR/items.json
 
+echo "Setup autorun: remap Caps-Esc, SSH forward agent"
 rm -rf ~/.config/autorun
 ln -s ${PWD}/autorun ~/.config/autorun
+
+rm ~/Library/LaunchAgents/com.user.loginscript.plist
+cp ${PWD}/autorun/com.user.loginscript.plist ~/Library/LaunchAgents/com.user.loginscript.plist
+launchctl load ~/Library/LaunchAgents/com.user.loginscript.plist
+
+echo "Done"
