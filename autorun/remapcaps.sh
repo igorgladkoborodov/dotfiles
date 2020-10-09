b548300e8b482b254116471a38c273d4d4b48947
Igor Gladkoborodov <igor.gladkoborodov@gmail.com>
2020-10-08 17:47:58 -0700

    Setup Karabiner

--- autorun/remapcaps.sh
+++ autorun/remapcaps.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029},{"HIDKeyboardModifierMappingSrc":0x700000029,"HIDKeyboardModifierMappingDst":0x700000039},]}'
+# Replace Caps and Esc.
+# Don't need it any more, it is handled by Karabiner
+# hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000029,"HIDKeyboardModifierMappingDst":0x700000029},{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000039},]}'
