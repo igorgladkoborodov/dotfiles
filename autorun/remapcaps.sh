2bdb6d6af1133ce1698176c4e22a797cef58b273
Igor Gladkoborodov <igor.gladkoborodov@gmail.com>
2020-10-08 19:46:12 -0700

    remap keys Esc -> Shift, Caps -> Esc

--- autorun/remapcaps.sh
+++ autorun/remapcaps.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029},{"HIDKeyboardModifierMappingSrc":0x700000029,"HIDKeyboardModifierMappingDst":0x700000039},]}'
+# https://developer.apple.com/library/archive/technotes/tn2450/_index.html#//apple_ref/doc/uid/DTS40017618-CH1-TNTAG7
+# Caps -> Esc
+# Esc -> Shift
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029},{"HIDKeyboardModifierMappingSrc":0x700000029,"HIDKeyboardModifierMappingDst":0x7000000E1},]}'
